65a4722a7dd1d8add202d7a30c7ef108f84002f1
ZarikZa <schemetov.ya@yandex.ru>
2024-03-30 09:31:01 +0300

    postavte 5

---
+++ Unitaz.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PervayaLabaDataSet.Laba1DataSetTableAdapters;
+
+namespace PervayaLabaDataSet
+{
+    /// <summary>
+    /// Логика взаимодействия для Unitaz.xaml
+    /// </summary>
+    public partial class Unitaz : Page
+    {
+        UnitazsTableAdapter UnitazsTableAdapter = new UnitazsTableAdapter();
+        public Unitaz()
+        {
+            InitializeComponent();
+            datygridy.ItemsSource = UnitazsTableAdapter.GetData();
+        }
+    }
+}
---
+++ UnitazasTypes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PervayaLabaDataSet.Laba1DataSetTableAdapters;
+
+namespace PervayaLabaDataSet
+{
+    /// <summary>
+    /// Логика взаимодействия для UnitazasTypes.xaml
+    /// </summary>
+    public partial class UnitazasTypes : Page
+    {
+        UnitazTypesTableAdapter UnitazTypesTableAdapter = new UnitazTypesTableAdapter();
+        public UnitazasTypes()
+        {
+            InitializeComponent();
+            datygridy.ItemsSource = UnitazTypesTableAdapter.GetData();
+        }
+    }
+}
